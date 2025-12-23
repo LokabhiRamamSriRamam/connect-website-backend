c450b19e950867e69dd37077ca305579a74be0e2
LokabhiRamamSriRamam <avtanshg919@gmail.com>
2025-12-23 16:39:27 +0530

    dotenv fix serverless function

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-import dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
+// import dotenv from "dotenv";
+// import express from "express";
+// import cors from "cors";
 
-import connectDB from "./api/utils/db.js";
-import geminiRoutes from "./api/routes/gemini.routes.js";
-import leadRoutes from "./api/routes/leads.routes.js";
-import careerRoutes from "./api/routes/careers.routes.js";
+// import connectDB from "./api/utils/db.js";
+// import geminiRoutes from "./api/routes/gemini.routes.js";
+// import leadRoutes from "./api/routes/leads.routes.js";
+// import careerRoutes from "./api/routes/careers.routes.js";
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
+// const app = express();
 
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-connectDB();
+// connectDB();
 
-app.get("/", (req, res) => {
-  res.json({ status: "Local backend running 🟢" });
-});
+// app.get("/", (req, res) => {
+//   res.json({ status: "Local backend running 🟢" });
+// });
 
-app.use("/api/gemini", geminiRoutes);
-app.use("/api/leads", leadRoutes);
-app.use("/api/careers", careerRoutes);
+// app.use("/api/gemini", geminiRoutes);
+// app.use("/api/leads", leadRoutes);
+// app.use("/api/careers", careerRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`🚀 Local server running on http://localhost:${PORT}`)
-);
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () =>
+//   console.log(`🚀 Local server running on http://localhost:${PORT}`)
+// );
